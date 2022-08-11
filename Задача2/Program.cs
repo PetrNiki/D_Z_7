8b37e0c0f687abe45cd2a6bdee269ff17d396338
PetrNiki <nikitenkopv@yandex.ru>
2022-08-11 12:28:10 +0300

    Задача2

--- Задача2/Program.cs
+++ Задача2/Program.cs
@@ -3,46 +3,46 @@
 // что такого элемента нет.
 
 void FillArray(int[,] array)
- {
-     for (int m = 0; m < array.GetLength(0); m++)
-     {
-         for (int n = 0; n < array.GetLength(1); n++)
-         {
-             array[m, n] = new Random().Next(1, 20);
-         }
-     }
- }
- void PrintArray(int[,] array)
- {
-     for (int m = 0; m < array.GetLength(0); m++)
-     {
+{
+    for (int m = 0; m < array.GetLength(0); m++)
+    {
+        for (int n = 0; n < array.GetLength(1); n++)
+        {
+            array[m, n] = new Random().Next(1, 20);
+        }
+    }
+}
+void PrintArray(int[,] array)
+{
+    for (int m = 0; m < array.GetLength(0); m++)
+    {
          for (int n = 0; n < array.GetLength(1); n++)
          {
              Console.Write($"{array[m, n]} ");
          }
          Console.WriteLine();
-     }
- }
- int m = 5;
- int n = 2;
- int[,] array = new int[m, n];
+    }
+}
+int m = 5;
+int n = 2;
+int[,] array = new int[m, n];
 
- FillArray(array);
- PrintArray(array);
+FillArray(array);
+PrintArray(array);
 
- Console.WriteLine("Введите число: ");
- int num = Convert.ToInt32(Console.ReadLine());
- string Position(int[,] array)
- {
-     string result = string.Empty;
-     for (int m = 0; m < array.GetLength(0); m++)
-     {
-         for (int n = 0; n < array.GetLength(1); n++)
-         {
-             if (num == array[m, n]) result += $"({m}, {n})";
-         }
-     }
-     if (result == string.Empty) result = "Такого элемента нет";
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+string Position(int[,] array)
+{
+    string result = string.Empty;
+    for (int m = 0; m < array.GetLength(0); m++)
+    {
+        for (int n = 0; n < array.GetLength(1); n++)
+        {
+            if (num == array[m, n]) result += $"({m}, {n})";
+        }
+    }
+    if (result == string.Empty) result = "Такого элемента нет";
     return result;
- }
- Console.WriteLine(Position(array));
+}
+Console.WriteLine(Position(array));
