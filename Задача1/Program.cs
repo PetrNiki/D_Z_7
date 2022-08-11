14d6791c3b49f6f0f5c733bdd97b48edb90b6e18
PetrNiki <nikitenkopv@yandex.ru>
2022-08-11 07:47:23 +0300

    Задача1

---
+++ Задача1/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте двумерный массив размером m×n, 
+//заполненный случайными вещественными числами.
+void FillArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+           array[i, j] = new Random().NextDouble(); 
+        }
+    }
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+       for (int j = 0; j < array.GetLength(1); j++)
+       {
+           Console.Write($"{array[i, j]}");
+       } 
+       Console.WriteLine();
+    }
+}
+int m = 5;
+int n = 2;
+double[,] array = new double[m, n];
+
+FillArray(array);
+PrintArray(array);
